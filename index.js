b2d407b739d624497cd62e031508f7b32aa37543
Eric Krukowski <erickruk@amazon.com>
2023-04-03 04:43:31 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let index =0;index<word.length/2;index++){ 
+    if (word.charAt(index)!=word.charAt(word.length-index-1)) return false
+  }
+  return true
 }
 
 /* 
-  Add your pseudocode here
+ loop through word starting at the first character
+    if character at index !=  character at end - index return false
+  if all checks out, return true
 */
 
 /*
-  Add written explanation of your solution here
+  only need to look at first half of word, since you're checking against back half
+  middle character on odd lengths don't need checked, so length/2 is fine to cut out the middle
+  returning as soon as something is wrong prevents cycling through entire loop
 */
 
 // You can run `node index.js` to view these console logs
